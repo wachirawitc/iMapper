db2a77bdeeaa961fc312e64050aa89513a63555f
Wachirawit <wachirawit.chankaew@gmail.com>
2017-05-07 18:30:08 +0700

    Update repository form

--- iMapper/iMapper/Forms/RepositoryForm.Designer.cs
+++ iMapper/iMapper/Forms/RepositoryForm.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.ColumnNumber = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.IsReplace = new System.Windows.Forms.CheckBox();
@@ -109,32 +109,32 @@
             this.Options.FormattingEnabled = true;
             this.Options.Location = new System.Drawing.Point(14, 346);
             this.Options.Name = "Options";
-            this.Options.Size = new System.Drawing.Size(316, 24);
+            this.Options.Size = new System.Drawing.Size(415, 24);
             this.Options.TabIndex = 20;
             // 
             // Columns
             // 
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.Columns.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Columns.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.Columns.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.Columns.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.Columns.DefaultCellStyle = dataGridViewCellStyle4;
             this.Columns.Location = new System.Drawing.Point(0, 77);
             this.Columns.Name = "Columns";
             this.Columns.ReadOnly = true;
-            this.Columns.Size = new System.Drawing.Size(342, 237);
+            this.Columns.Size = new System.Drawing.Size(429, 237);
             this.Columns.TabIndex = 17;
             // 
             // Tables
@@ -144,7 +144,7 @@
             this.Tables.FormattingEnabled = true;
             this.Tables.Location = new System.Drawing.Point(12, 21);
             this.Tables.Name = "Tables";
-            this.Tables.Size = new System.Drawing.Size(316, 24);
+            this.Tables.Size = new System.Drawing.Size(417, 24);
             this.Tables.TabIndex = 16;
             this.Tables.SelectedIndexChanged += new System.EventHandler(this.OnSelectedTables);
             // 
@@ -187,12 +187,14 @@
             this.EntityFrameworkName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
             this.EntityFrameworkName.Location = new System.Drawing.Point(14, 392);
             this.EntityFrameworkName.Name = "EntityFrameworkName";
-            this.EntityFrameworkName.Size = new System.Drawing.Size(316, 22);
+            this.EntityFrameworkName.Size = new System.Drawing.Size(415, 22);
             this.EntityFrameworkName.TabIndex = 26;
             // 
             // IsPluralize
             // 
             this.IsPluralize.AutoSize = true;
+            this.IsPluralize.Checked = true;
+            this.IsPluralize.CheckState = System.Windows.Forms.CheckState.Checked;
             this.IsPluralize.Location = new System.Drawing.Point(14, 420);
             this.IsPluralize.Name = "IsPluralize";
             this.IsPluralize.Size = new System.Drawing.Size(65, 17);

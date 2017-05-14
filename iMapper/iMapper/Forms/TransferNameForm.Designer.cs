b56953d6e06b2c913f7597ea5f431f3cf2a8b3d1
Wachirawit <wachirawit.chankaew@gmail.com>
2017-05-14 16:45:49 +0700

    Resize form

--- iMapper/iMapper/Forms/TransferNameForm.Designer.cs
+++ iMapper/iMapper/Forms/TransferNameForm.Designer.cs
@@ -40,14 +40,14 @@
             this.abnormalDatas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.abnormalDatas.Location = new System.Drawing.Point(12, 12);
             this.abnormalDatas.Name = "abnormalDatas";
-            this.abnormalDatas.Size = new System.Drawing.Size(352, 254);
+            this.abnormalDatas.Size = new System.Drawing.Size(660, 272);
             this.abnormalDatas.TabIndex = 0;
             // 
             // LoadButton
             // 
             this.LoadButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.LoadButton.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LoadButton.Location = new System.Drawing.Point(164, 273);
+            this.LoadButton.Location = new System.Drawing.Point(471, 291);
             this.LoadButton.Margin = new System.Windows.Forms.Padding(4);
             this.LoadButton.Name = "LoadButton";
             this.LoadButton.Size = new System.Drawing.Size(200, 34);
@@ -60,7 +60,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(379, 314);
+            this.ClientSize = new System.Drawing.Size(684, 338);
             this.Controls.Add(this.LoadButton);
             this.Controls.Add(this.abnormalDatas);
             this.Font = new System.Drawing.Font("Segoe UI Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
